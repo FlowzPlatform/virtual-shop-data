0e999ea5f359779a600e4d16ad2627e17ad813d9
Sanjaysinh Zala <sanjaysinhz@officebrain.com>
2018-03-21 19:01:48 +0530

    issue #34 #54

--- client/src/views/Main.vue
+++ client/src/views/Main.vue
@@ -59,7 +59,7 @@
                     
                     
                     <full-screen v-model="isFullScreen" @on-change="fullscreenChange"></full-screen>
-                    <lock-screen></lock-screen>
+                    <!--lock-screen></lock-screen-->
                     <!-- <message-tip v-model="mesCount"></message-tip> -->
                     <theme-switch></theme-switch>
 
@@ -106,7 +106,7 @@
     import tagsPageOpened from './main-components/tags-page-opened.vue';
     import breadcrumbNav from './main-components/breadcrumb-nav.vue';
     import fullScreen from './main-components/fullscreen.vue';
-    import lockScreen from './main-components/lockscreen/lockscreen.vue';
+/*     import lockScreen from './main-components/lockscreen/lockscreen.vue';*/
     import messageTip from './main-components/message-tip.vue';
     import themeSwitch from './main-components/theme-switch/theme-switch.vue';
     import Cookies from 'js-cookie';
@@ -120,7 +120,7 @@
             tagsPageOpened,
             breadcrumbNav,
             fullScreen,
-            lockScreen,
+            /* lockScreen, */
             messageTip,
             themeSwitch
         },
