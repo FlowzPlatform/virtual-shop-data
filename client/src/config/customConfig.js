d9d1fcb7a63e4c67d16c2573d7096404c784a330
Sanjaysinh Zala <sanjaysinhz@officebrain.com>
2018-03-29 18:00:41 +0530

    Solved #39, update social login and solve other general issue

--- client/src/config/customConfig.js
+++ client/src/config/customConfig.js
@@ -1,18 +1,19 @@
-let frontEndUrl = 'http://vshopdata.'+process.env.domainkey+'/#/login';
+let frontEndUrl = 'https://vshopdata.' + process.env.domainkey + '/#/login';
 
 module.exports =  {
   default : {
     loginUrl : "https://api."+process.env.domainkey+"/auth/api/login",
-    ldapLoginUrl : "https://auth."+process.env.domainkey+"/api/ldapauth",
-    registrationUrl : "https://api."+process.env.domainkey+"/serverapi/register",
-    signupUrl: "https://auth."+process.env.domainkey+"/api/setup",
-    userDetail: "https://auth."+process.env.domainkey+"/api/userdetails",
-    forgotPasswordUrl : "https://api."+process.env.domainkey+"/auth/api/forgetpassword",
-    resetpassword : "https://api."+process.env.domainkey+"/auth/api/resetpassword",
-    resetPasswordRedirectUrl: "https://www.vshopdata." + process.env.domainkey + "/reset-password",
+    ldapLoginUrl : "https://api." + process.env.domainkey + "/auth/api/ldapauth",
+    registrationUrl : "https://api." + process.env.domainkey + "/auth/serverapi/register",
+    signupUrl: "https://api." + process.env.domainkey + "/auth/api/setup",
+    userDetail: "https://api." + process.env.domainkey + "/auth/api/userdetails",
+    forgotPasswordUrl : "https://api." + process.env.domainkey + "/auth/api/forgetpassword",
+    resetPasswordUrl : "https://api." + process.env.domainkey + "/auth/api/resetpassword",
+    resetPasswordRedirectUrl: "https://vshopdata." + process.env.domainkey + "/#/reset-password",
+    changepassword: "https://api." + process.env.domainkey + "/auth/api/changepassword",
     feathersServiceBaseUrl : "https://api."+process.env.domainkey+"/pdmnew",     // live api
     // feathersServiceBaseUrl : "http://localhost:3038",                        // local api
-    varifyEmailUrl : "https://auth."+process.env.domainkey+"/api/verifyemail",
+    varifyEmailUrl: "https://api." + process.env.domainkey + "/auth/api/verifyemail",
     socketURI: 'wss://ws.' + process.env.domainkey + ':4038',                    // live socket
     // socketURI: 'ws://localhost:4038',                                        // local socket
 
@@ -23,17 +24,26 @@ module.exports =  {
     githubSuccessCallbackUrl: frontEndUrl,
     linkedInSuccessCallbackUrl: frontEndUrl,
 
-    flowzDashboardUrl : 'https://dashboard.'+process.env.domainkey,
-    flowzBuilderUrl : 'https://webbuilder.'+process.env.domainkey ,
-    flowzVmailUrl : 'https://vmail.'+process.env.domainkey ,
-    flowzUploaderUrl : 'https://uploader.'+process.env.domainkey ,
-    flowzDbetlUrl : 'https://dbetl.'+process.env.domainkey , 
+    flowzDashboardUrl: 'https://www.dashboard.' + process.env.domainkey,
+    flowzBuilderUrl: 'https://www.webbuilder.' + process.env.domainkey,
+    flowzVmailUrl: 'https://www.vmail.' + process.env.domainkey,
+    flowzUploaderUrl: 'https://www.uploader.' + process.env.domainkey,
+    flowzDbetlUrl: 'https://www.dbetl.' + process.env.domainkey, 
     
     loginWithFacebookUrl : 'https://auth.'+process.env.domainkey+'/auth/facebook',
-    loginWithGoogleUrl : 'https://auth.'+process.env.domainkey+'/auth/Gplus',
+    loginWithGoogleUrl : 'https://auth.'+process.env.domainkey+'/auth/google',
     loginWithTwitterUrl : 'https://auth.'+process.env.domainkey+'/auth/twitter',
     loginWithGithubUrl : 'https://auth.'+process.env.domainkey+'/auth/github',
     loginWithLinkedInUrl : 'https://auth.'+process.env.domainkey+'/auth/linkedin',
+
+    // loginWithFacebookUrl : 'https://auth.'+process.env.domainkey+'/auth/facebook',
+    // loginWithGoogleUrl : 'https://auth.'+process.env.domainkey+'/auth/Gplus',
+    // loginWithTwitterUrl : 'https://auth.'+process.env.domainkey+'/auth/twitter',
+    // loginWithGithubUrl : 'https://auth.'+process.env.domainkey+'/auth/github',
+    // loginWithLinkedInUrl : 'https://auth.'+process.env.domainkey+'/auth/linkedin',
+
+    subscriptionUrl: 'https://api.' + process.env.domainkey + '/subscription/',
+    usersubscriptionUrl: 'https://api.' + process.env.domainkey + '/subscription/user-subscription'
   }
 }
 
